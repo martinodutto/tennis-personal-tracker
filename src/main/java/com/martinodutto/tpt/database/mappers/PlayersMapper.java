9998f9e99669f8b9df37d08538b4db377c65e3b6
martinodutto <martinodutto@gmail.com>
2017-12-06 22:04:59 +0100

    Added Google's jsr305 artifact to the dependencies of the project, to allow the use of the javax.annotations package annotations (e.g. @Nullable, @Nonnull). Added EnableJspInEmbeddedTomcat bean, which is a workaround for the embedded Tomcat (run with the 'development' profile) to correctly locate the JSPs. This comes together with the need of the following two artifacts: tomcat-embed-jasper and jstl.

--- src/main/java/com/martinodutto/tpt/database/mappers/PlayersMapper.java
+++ src/main/java/com/martinodutto/tpt/database/mappers/PlayersMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 @Mapper
@@ -13,6 +14,7 @@ public interface PlayersMapper {
 
     List<Player> selectAll();
 
+    @Nullable
     Player selectByPk(@Param("playerId") int playerId);
 
     int insert(@Param("player") Player player);
