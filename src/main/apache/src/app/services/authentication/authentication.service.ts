e4cda1d985f0bd0032783544b6674306189a75a2
martinodutto <martinodutto@gmail.com>
2018-01-02 22:23:53 +0100

    Solved a problem with Unicode when encoding to Base64 (thank you, English-speaking world, for never thinking that there are more than 128 characters in the human language!).

--- src/main/apache/src/app/services/authentication/authentication.service.ts
+++ src/main/apache/src/app/services/authentication/authentication.service.ts
@@ -9,7 +9,8 @@ export class AuthenticationService {
   private URL_PREFIX: string = "../script/authentication";
   private JWT_TOKEN_PARAM_NAME = "jwt";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   saveJwtToken(token: string): void {
     localStorage.setItem(this.JWT_TOKEN_PARAM_NAME, token);
@@ -20,11 +21,11 @@ export class AuthenticationService {
   }
 
   register(user: User): Observable<any> {
-    return this.http.post(this.URL_PREFIX + "/register", this.encodeToBase64(user));
+    return this.http.post(this.URL_PREFIX + "/register", AuthenticationService.encodeToBase64(user));
   }
 
   login(user: User): Observable<any> {
-    return this.http.post(this.URL_PREFIX + "/login", this.encodeToBase64(user));
+    return this.http.post(this.URL_PREFIX + "/login", AuthenticationService.encodeToBase64(user));
   }
 
   logout(): void {
@@ -35,10 +36,32 @@ export class AuthenticationService {
     return this.getJwtToken() !== null;
   }
 
-  private encodeToBase64(user: User): User {
-    user.username = btoa(user.username);
-    user.password = btoa(user.password);
+  private static encodeToBase64(user: User): User {
+    user.username = this.b64EncodeUnicode(user.username);
+    user.password = this.b64EncodeUnicode(user.password);
 
     return user;
+  }
+
+  /**
+   * Function to convert from UTF8 to Base64 solving the Unicode Problem
+   * Requires: window.btoa and window.encodeURIComponent functions
+   * More info: http://stackoverflow.com/questions/30106476/using-javascripts-atob-to-decode-base64-doesnt-properly-decode-utf-8-strings
+   * Samples:
+   *      b64EncodeUnicode('✓ à la mode'); // "4pyTIMOgIGxhIG1vZGU="
+   *      b64EncodeUnicode('\n'); // "Cg=="
+   */
+  private static b64EncodeUnicode(str: string): string {
+    if (window
+      && "btoa" in window
+      && "encodeURIComponent" in window) {
+      return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (match, p1) => {
+        return String.fromCharCode(("0x" + p1) as any);
+      }));
+    } else {
+      console.warn("b64EncodeUnicode requirements: window.btoa and window.encodeURIComponent functions");
+      return null;
+    }
+
   }
 }
