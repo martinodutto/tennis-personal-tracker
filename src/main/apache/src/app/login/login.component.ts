989339c66db0e182e090c1ba2ef67c2ba7fe5b2a
martinodutto <martinodutto@gmail.com>
2018-01-21 11:24:57 +0100

    Added a spinner icon to the login button when loading.

--- src/main/apache/src/app/login/login.component.ts
+++ src/main/apache/src/app/login/login.component.ts
@@ -13,12 +13,14 @@ export class LoginComponent implements OnInit {
 
   form: FormGroup;
   loginErrorMessage: string;
+  loading: boolean;
 
   constructor(private formBuilder: FormBuilder,
               private router: Router,
               private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    this.loading = false;
     this.form = this.formBuilder.group({
       username: new FormControl('', [Validators.required, Validators.maxLength(64)]),
       password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(255)])
@@ -28,13 +30,16 @@ export class LoginComponent implements OnInit {
   login() {
     console.debug('Logging in!');
     this.loginErrorMessage = null; // reset, in case an error message was already displayed
+    this.loading = true;
     this.authenticationService.login(
       new User(this.form)
     ).subscribe(response => {
+      this.loading = false;
       // we set the JWT on the local storage, where it can be retrieved by subsequent requests
       this.authenticationService.saveJwtTokenAndUsername(response.token, this.form.value['username']);
       this.router.navigate(['home']);
     }, error => {
+      this.loading = false;
       console.error(`Error while logging in: ${error.message}`);
       this.loginErrorMessage = error.error ? error.error.message || 'Error while logging in' : 'Error while logging in';
     });
