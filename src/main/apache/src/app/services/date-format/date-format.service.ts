5e691b7a276f52183affc2e61cb02b6ca85971a9
martinodutto <martinodutto@gmail.com>
2018-11-03 20:46:47 +0100

    Fix ISO date formatting; unambiguous definition for view

--- src/main/apache/src/app/services/date-format/date-format.service.ts
+++ src/main/apache/src/app/services/date-format/date-format.service.ts
@@ -8,10 +8,15 @@ export class DateFormatService {
 
   constructor() { }
 
+  public static padNumber(n: number, length: number): string {
+    return (n ? n.toString() : '').padStart(length, '0');
+  }
+
   public isoFormat(date: NgbDateStruct): string {
     let formattedDate: string = null;
     if (date) {
-      formattedDate = `${date.year}-${date.month}-${date.day}`;
+      formattedDate = `${DateFormatService.padNumber(date.year, 4)}-` +
+        `${DateFormatService.padNumber(date.month, 2)}-${DateFormatService.padNumber(date.day, 2)}`;
     }
     return formattedDate;
   }
--- src/main/apache/src/app/services/time-format/time-format.service.ts
+++ src/main/apache/src/app/services/time-format/time-format.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {NgbTimeStruct} from '@ng-bootstrap/ng-bootstrap';
+import {DateFormatService} from '../date-format/date-format.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,12 @@ export class TimeFormatService {
   constructor() {
   }
 
-  private static padNumber(n: number, length: number): string {
-    return (n ? n.toString() : '').padStart(length, '0');
-  }
-
   public format(time: NgbTimeStruct): string {
     let formattedTime: string = null;
     if (time) {
-      formattedTime = `${TimeFormatService.padNumber(time.hour, 2) || '00'}:` +
-        `${TimeFormatService.padNumber(time.minute, 2) || '00'}:` +
-        `${TimeFormatService.padNumber(time.second, 2) || '00'}`;
+      formattedTime = `${DateFormatService.padNumber(time.hour, 2) || '00'}:` +
+        `${DateFormatService.padNumber(time.minute, 2) || '00'}:` +
+        `${DateFormatService.padNumber(time.second, 2) || '00'}`;
     }
     return formattedTime;
   }
